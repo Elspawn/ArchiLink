a6a36aca46ebc85ec990c071b995afb7f1fd1856
Bodin Tanguy <127230030+TanguyBod@users.noreply.github.com>
2026-08-12 11:55:20 +0200

    Add error handling for optional death cause
    Handle optional 'cause' field in DeathLink messages.

--- src/archipelago/bot_client.py
+++ src/archipelago/bot_client.py
@@ -108,7 +108,10 @@ If it's not related to archipelago.gg inactivity, it is the self-hosted instance
             self.logger.debug(f"Processing DeathLink message")
             dead_player_name = message["data"]['source']
             death_time = message["data"]['time']
-            death_cause = message["data"]['cause']
+            try :
+                death_cause = message["data"]['cause'] #Cause is an optionnal field in this message
+            except Exception as e :
+                death_case = "Died"
             if self.custom_deathlink_flavor :
                 msg = get_deathlink_flavor(dead_player_name, death_time)
             else :
